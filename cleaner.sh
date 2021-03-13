dbf90d8cfe40e87bff246150ddad8d8f4452ff7a
cuppajoeman <ccn@cuppajoeman.com>
2021-03-13 00:17:37 -0500

    add in print statement if no files need to be cleaned

--- cleaner.sh
+++ cleaner.sh
@@ -3,15 +3,16 @@ shopt -s extglob nullglob
 files=(!(.git|cleaner.sh|*.tex|*.sty|*.pdf|output.txt|new.sh))
 
 # ${#files[@]} is the number of entries in the file array
-(( "${#files[@]}" > 0 )) && {
-	printf 'files to be deleted:\n'
-	printf '%s\n' "${files[@]}"
+if (( ${#files[@]} > 0 )); then
+ printf 'files to be deleted:\n'
+ printf '%s\n' "${files[@]}"
   # Ask for confirmation
   read -p "Are you sure? " -n 1 -r
   echo    # (optional) move to a new line
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
     rm -- "${files[@]}"
   fi
-}
+else
+ printf 'no files found to be deleted:\n'
+fi
 
