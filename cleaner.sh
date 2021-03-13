7c7d9ac2328d2d55574d6f556a81556ab6be7755
cuppajoeman <ccn@cuppajoeman.com>
2021-03-13 00:06:52 -0500

    the vacuum cleaner man, he's seen my ...

--- cleaner.sh
+++ cleaner.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-shopt -s extglob
-#FILES=".git|cleaner.sh|add_structure.sh|*.tex|*.sty|*.pdf|output.txt|new.sh"
-#echo !("$FILES")
-rm !(.git|cleaner.sh|add_structure.sh|*.tex|*.sty|*.pdf|output.txt|new.sh)
+shopt -s extglob nullglob
+files=(!(.git|cleaner.sh|*.tex|*.sty|*.pdf|output.txt|new.sh))
+
+# ${#files[@]} is the number of entries in the file array
+(( "${#files[@]}" > 0 )) && {
+	printf 'files to be deleted:\n'
+	printf '%s\n' "${files[@]}"
+  # Ask for confirmation
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    rm -- "${files[@]}"
+  fi
+}
+
