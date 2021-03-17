0993588cca773ba9e2bc666f7af19ab5cd62640c
cuppajoeman <ccn@cuppajoeman.com>
2021-03-17 15:49:05 -0400

    add work to kgbase-client wrapper

--- content_to_knowledge_graph.py
+++ content_to_knowledge_graph.py
@@ -22,13 +22,21 @@ project_tables = project_schema['tables']
 
 tables = {}
 
+# Update text representations of kg
+
 for table_data in project_tables:
     name = table_data['displayName']
     with open("kgbase_data/" + name + ".txt", 'w') as f:
+        f.write("Table Name: " + name + ", ID: " + table_data['tableId'] + '\n')
         s = pprint.pformat(q.get_graph(project_id, table_data['tableId']))
         f.write(s)
 
+# Create a new node
 
+table_to_id = {
+        'Structure':  'tab-MVVF6vQI4G2jDqgcLJM',
+        'Knowledge': 'tab-MVVDy_ojQuAFVL6HAK1'
+        }
 
 values = {
     'col-0': 'Test',
@@ -36,21 +44,20 @@ values = {
     'col-7': True
 }
 
-#("column8", "row-M587jZETRpuCBIXUfw6"),
 edges = [
         # Property - do this dynamically next time
         ("Type", "row-MVrlg2B1NM57XhEZeZm"),
-        ("Status", "row-MVrlg2B1NM57XhEZeZm")
+        ("Status", "row-MVXhdD9Mbtt901kMYRX"),
+        ("Parent", "row-MVVGSO2GHfyB6ZfxkhD")
 ]
 
-#result = q.create_vertex(
-#    project_id,
-#    kt_id,
-#    values,
-#    edges
-#)
-#
-#print(result)
+result = q.create_vertex(
+    project_id,
+    table_to_id['Knowledge'],
+    values,
+    edges
+)
+
 
 # Title = 
 # Type = 
@@ -69,45 +76,3 @@ edges = [
 # col-6 Knowledge Used
 # col-7 Formatted
 
- # OUTPUT OF TYPE TABLE
- #'vertices': [{'contextId': None,
- #              'id': 'row-MVXSSn_vJuk_qhboEni',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Theorem'}]},
- #             {'contextId': None,
- #              'id': 'row-MVXSSn_vJuk_qhboEnj',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Definition'}]},
- #             {'contextId': None,
- #              'id': 'row-MVc4yXdAM-raLkjVgZp',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Device'}]},
- #             {'contextId': None,
- #              'id': 'row-MVc51vyjRNGEHuiVDQ6',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Machine'}]},
- #             {'contextId': None,
- #              'id': 'row-MVoAVO12AKScs-d2ECR',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Proposition'}]},
- #             {'contextId': None,
- #              'id': 'row-MVrlg2B1NM57XhEZeZm',
- #              'label': 'tab-MVXSHAoNLpA0Cc-atfi',
- #              'values': [{'key': 'col-0', 'value': 'Property'}]}]}
-
-# OUTPUT OF STATUS TABLE
-#{'edges': [],
-# 'total': 2,
-# 'vertexIds': ['row-MVXhdD9Mbtt901kMYRX', 'row-MVXhee-z7QrOpFkA0FX'],
-# 'vertices': [{'contextId': None,
-#               'id': 'row-MVXhdD9Mbtt901kMYRX',
-#               'label': 'tab-MVXh1EVi97AWenYKjUp',
-#               'values': [{'key': 'col-0', 'value': 'Complete'}]},
-#              {'contextId': None,
-#               'id': 'row-MVXhee-z7QrOpFkA0FX',
-#               'label': 'tab-MVXh1EVi97AWenYKjUp',
-#               'values': [{'key': 'col-0', 'value': 'Work In Progress'}]}]}
-
-
-#pprint(q.get_graph(project_id, tables['Type']['tableId']))
-#pprint(q.get_graph(project_id, kt_id))
