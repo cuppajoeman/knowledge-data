24642d1d38d48cf6b5847264194b846ecabd0531
cuppajoeman <ccn@cuppajoeman.com>
2021-03-17 20:13:06 -0400

    turn kg representation into basic title <-> id mapping

--- content_to_knowledge_graph.py
+++ content_to_knowledge_graph.py
@@ -20,16 +20,46 @@ project_schema = q.get_schema(project_id)
 
 project_tables = project_schema['tables']
 
+# The first layer is a directory of the tables, the second layer shows the rows of the table and their associated ids
 tables = {}
 
 # Update text representations of kg
 
+cols = ["Type", "Status", "Parent", "Knowledge_Used"]
+
+
+types_to_id = {}
+
+# A vertex represents a row, they are the same thing
+
 for table_data in project_tables:
-    name = table_data['displayName']
-    with open("kgbase_data/" + name + ".txt", 'w') as f:
-        f.write("Table Name: " + name + ", ID: " + table_data['tableId'] + '\n')
+    table_name = table_data['displayName']
+    table_id = table_data['tableId'] 
+
+    # Instantiate table to empty dictionary
+    tables[table_name] = {}
+
+    vertices = q.get_graph(project_id, table_id)['vertices']
+    #pprint.pprint(vertices)
+    # Generate the table 
+    for row in vertices:
+        pprint.pprint(row)
+        # This code will break when we have two vertices with the same name
+        for row_entry in row['values']:
+            # We are looking at it's title
+            if row_entry['key'] == 'col-0':
+                title = row_entry['value']
+                tables[table_name][title] = row['id']
+                # Exits inner loop, outer continues
+                break
+
+    # Also write it to a file, because why not
+    with open("kgbase_data/" + table_name + ".txt", 'w') as f:
+        f.write("Table Name: " + table_name + ", ID: " + table_data['tableId'] + '\n')
         s = pprint.pformat(q.get_graph(project_id, table_data['tableId']))
         f.write(s)
+
+pprint.pprint(tables)
 
 # Create a new node
 
@@ -51,12 +81,12 @@ edges = [
         ("Parent", "row-MVVGSO2GHfyB6ZfxkhD")
 ]
 
-result = q.create_vertex(
-    project_id,
-    table_to_id['Knowledge'],
-    values,
-    edges
-)
+#result = q.create_vertex(
+#    project_id,
+#    table_to_id['Knowledge'],
+#    values,
+#    edges
+#)
 
 
 # Title = 
---
+++ row_args.py
@@ -0,0 +1,15 @@
+# Title = 
+# Type = 
+# Content = 
+# Status =  
+# Parent = 
+# Knowledge_Used = 
+# Formatted = 
+
+Title = 
+Type = 
+Content = 
+Status =  
+Parent = 
+Knowledge_Used = 
+Formatted = 
