45902bab1f46c5b1619ce7b2a7083a9e7dddc889
cuppajoeman <ccn@cuppajoeman.com>
2021-04-20 19:38:56 -0400

    add in vector

--- add_id.sh
+++ add_id.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Input: The term that you want an id for, the filename which will be renamed - no extension
 
-
 grep $1 kgbase_data/tables.txt 
 
 echo "Now copy paste the id you want: (everything after row-)"
